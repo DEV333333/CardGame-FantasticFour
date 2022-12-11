f8ff828ef0c2f8ffa6046412701ffe054e7ecee0
Hriday Juneja <111471809+DEV333333@users.noreply.github.com>
2022-12-11 11:11:20 -0500

    We're done

--- src/cardgame/Values.java
+++ src/cardgame/Values.java
@@ -1,3 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package softwareproject;
+
+/**
+ *
+ * @author Hriday Juneja
+ * @author Lovepreet Kaur
+ * @author Nikhil Chaudhary
+ * @author Harsh Patel
+ */
+
 package softwareproject;
 
 public enum Values {
